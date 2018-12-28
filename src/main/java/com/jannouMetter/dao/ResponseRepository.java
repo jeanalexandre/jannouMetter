e77691bcf5d5c2cceac779c314509564bb772927
Jannou <jannou59@gmail.com>
2018-12-28 21:02:28 +0100

    refacto(responses): delete responses object

--- src/main/java/com/jannouMetter/dao/ResponseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jannouMetter.dao;
-
-import com.jannouMetter.bo.Response;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResponseRepository extends JpaRepository<Response, Long> {
-}
